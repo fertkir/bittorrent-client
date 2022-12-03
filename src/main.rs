e456903330afb44a1c1797969c9962672e686884
Kirill Fertikov <kirill.fertikov@gmail.com>
2022-12-03 19:09:45 +0500

    establishing handshakes with peers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,73 @@
+use std::io::{Read, Result, Write};
+use std::net::{SocketAddr, TcpStream};
+use std::time::Duration;
+
+use rand::distributions::{Alphanumeric, DistString};
+
 mod torrent;
 mod tracker;
 
+#[derive(Debug)]
+struct Handshake {
+    p_str: String,
+    info_hash: [u8; 20],
+    peer_id: Vec<u8>,
+}
 
 fn main() {
     let filepath = "/home/fertkir/Downloads/debian-11.5.0-amd64-netinst.iso.torrent";
     let torrent = torrent::parse(filepath);
-    let response = tracker::query(&torrent);
-    println!("{:?}", response);
+    let peer_id = generate_peer_id();
+    let tracker_response = tracker::query(&torrent, &peer_id);
+    println!("{:?}", tracker_response);
+    let handshake = Handshake {
+        p_str: "BitTorrent protocol".to_string(),
+        info_hash: torrent.info_hash,
+        peer_id: peer_id.into_bytes(),
+    };
+    let serialized_handshake = serialize(&handshake);
+
+    println!("{:?}", serialized_handshake);
+
+    for peer in tracker_response.peers {
+        match send_handshake(&peer, &serialized_handshake) {
+            Ok(response) => {
+                println!("{:?}", response)
+            }
+            Err(_) => {}
+        }
+    }
+}
+
+fn send_handshake(peer: &SocketAddr, handshake: &[u8]) -> Result<[u8; 68]> {
+    let timeout = Duration::new(3, 0);
+    let mut stream = TcpStream::connect_timeout(peer, timeout)?;
+    stream.set_read_timeout(Some(timeout))?;
+    stream.set_write_timeout(Some(timeout))?;
+    stream.write_all(handshake)?;
+    let mut buf = [0; 68];
+    stream.read_exact(&mut buf)?;
+    Ok(buf)
+}
+
+fn serialize(handshake: &Handshake) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
+    result.push(19);
+    result.extend(handshake.p_str.as_bytes());
+    result.extend([0; 8]);
+    result.extend(handshake.info_hash);
+    result.extend(handshake.peer_id.as_slice());
+    result
+}
+
+fn deserialize(handshake: &[u8; 68]) -> Handshake {
+    Handshake {
+        p_str: String::from("handshake[1..21]"),
+        info_hash: [0; 20], // handshake[21..42].try_into().unwrap()
+        peer_id: handshake[42..69].to_vec(),
+    }
+}
+
+fn generate_peer_id() -> String {
+    Alphanumeric.sample_string(&mut rand::thread_rng(), 20)
 }
--- src/tracker.rs
+++ src/tracker.rs
@@ -1,4 +1,4 @@
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use serde_derive::Deserialize;
 use serde_with::Bytes;
@@ -11,13 +11,7 @@ const PORT: i32 = 9876; // todo generate
 #[derive(Debug)]
 pub struct TrackerResponse {
     pub interval: u32,
-    pub peers: Vec<Peer>,
-}
-
-#[derive(Debug)]
-pub struct Peer {
-    pub ip: IpAddr,
-    pub port: u16,
+    pub peers: Vec<SocketAddr>,
 }
 
 #[serde_as]
@@ -28,33 +22,32 @@ struct TrackerResp {
     peers: Vec<u8>,
 }
 
-pub fn query(torrent: &TorrentFile) -> TrackerResponse {
-    let resp = query_tracker(torrent);
+pub fn query(torrent: &TorrentFile, peer_id: &str) -> TrackerResponse {
+    let resp = query_tracker(torrent, peer_id);
     TrackerResponse {
         interval: resp.interval,
         peers: parse_peers(&resp.peers),
     }
 }
 
-fn query_tracker(torrent: &TorrentFile) -> TrackerResp {
+fn query_tracker(torrent: &TorrentFile, peer_id: &str) -> TrackerResp {
     let url = torrent.announce.to_string() + "?uploaded=0&downloaded=0&compact=1\
         &info_hash=" + &urlencoding::encode_binary(&torrent.info_hash)
         + "&port=" + &PORT.to_string()
-        + "&peer_id=jfa68h4w7i8eghei8rdf" // todo generate
+        + "&peer_id=" + peer_id
         + "&left=" + &torrent.length.to_string();
     println!("{}", &url);
     let result = reqwest::blocking::get(&url).unwrap().bytes().unwrap();
     bt_bencode::from_slice(&result.as_ref()).unwrap()
 }
 
-fn parse_peers(peers: &[u8]) -> Vec<Peer> {
+fn parse_peers(peers: &[u8]) -> Vec<SocketAddr> {
     let mut result = Vec::new();
     let mut i = 0;
     while i < peers.len() {
-        result.push(Peer {
-            ip: IpAddr::V4(Ipv4Addr::new(peers[i], peers[i + 1], peers[i + 2], peers[i + 3])),
-            port: ((peers[i + 4] as u16) << 8) | peers[i + 5] as u16,
-        });
+        let ip = IpAddr::from(Ipv4Addr::new(peers[i], peers[i + 1], peers[i + 2], peers[i + 3]));
+        let port = ((peers[i + 4] as u16) << 8) | peers[i + 5] as u16;
+        result.push(SocketAddr::new(ip, port));
         i = i + 6
     }
     result
